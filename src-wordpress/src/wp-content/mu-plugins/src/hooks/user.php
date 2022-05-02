400e7231b834113c19bec22e89b43f278eeef5e8
YAOYINGYING <yao3060@gmail.com>
2022-05-02 16:49:27 +0800

    add subordinates to profile api

--- src-wordpress/src/wp-content/mu-plugins/src/hooks/user.php
+++ src-wordpress/src/wp-content/mu-plugins/src/hooks/user.php
@@ -9,24 +9,28 @@
  * @param WP_User          $user     User object used to create response.
  * @param WP_REST_Request  $request  Request object.
  */
-add_filter('rest_prepare_user', function ($response, $user, $request) {
-	$groups = wp_get_terms_for_user($user, 'user-group');
-	if ($groups && is_array($groups)) {
-		foreach ($groups as $key => $group) {
-			$response->data['stores'][] = [
-				'id' => $group->term_id,
-				'name' => $group->name,
-				'slug' => $group->slug
-			];
-		}
-	}
 
-	return $response;
+use App\Services\UserService;
+
+add_filter('rest_prepare_user', function ($response, $user, $request) {
+    $groups = wp_get_terms_for_user($user, 'user-group');
+    if ($groups && is_array($groups)) {
+        foreach ($groups as $key => $group) {
+            $response->data['stores'][] = [
+                'id' => $group->term_id,
+                'name' => $group->name,
+                'slug' => $group->slug
+            ];
+        }
+        $response->data['subordinates'] = UserService::getSubordinates($user);
+    }
+
+    return $response;
 }, 10, 3);
 
-add_filter('user_contactmethods', function( $methods, $user){
-	return [
-		'mobile_phone'    => __( 'Mobile Phone', 'cake' ),
-		'wechat'    => __( 'WeChat', 'cake' ),
-	];
+add_filter('user_contactmethods', function ($methods, $user) {
+    return [
+        'mobile_phone'    => __('Mobile Phone', 'cake'),
+        'wechat'    => __('WeChat', 'cake'),
+    ];
 }, 10, 2);
